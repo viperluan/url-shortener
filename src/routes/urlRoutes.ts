0b0729f989badb45d2123241786719b3686cd90c
viperluan <viperluan@gmail.com>
2024-08-15 01:00:29 -0300

    added middleware to url routes

--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import UrlController from '../controllers/UrlController';
+import AuthenticateJwtMiddleware from '../middlewares/AuthenticateJwtMiddleware';
 
 const urlRoutes = Router();
 
+const authenticateJwtMiddleware = new AuthenticateJwtMiddleware();
 const urlController = new UrlController();
 
 urlRoutes.get('/:url', urlController.getUrl);
-urlRoutes.post('/shorten', urlController.createUrl);
-urlRoutes.put('/shorten', urlController.editUrl);
-urlRoutes.delete('/shorten/:id', urlController.deleteUrl);
+urlRoutes.post('/', urlController.createUrl);
+urlRoutes.put('/', authenticateJwtMiddleware.middleware, urlController.editUrl);
+urlRoutes.delete('/:id', authenticateJwtMiddleware.middleware, urlController.deleteUrl);
 
 export default urlRoutes;
