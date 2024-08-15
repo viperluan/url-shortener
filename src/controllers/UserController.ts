3594c1f21a929440bd294c731c40761a654c559c
viperluan <viperluan@gmail.com>
2024-08-15 02:54:37 -0300

    remove list of user logic

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,5 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
-import ListDataUserService from '../services/ListDataUserService';
-
-interface IUserControllerRequest extends Request {
-  token?: {
-    id: string;
-    email: string;
-  };
-}
 
 export default class UserController {
   async createUser(request: Request, response: Response) {
@@ -21,16 +13,5 @@ export default class UserController {
     if (!user) return response.status(401).json({ message: 'This email is already in use.' });
 
     return response.status(201).json(user);
-  }
-
-  async getUserData(request: IUserControllerRequest, response: Response) {
-    if (!request.token) return response.status(401).json({});
-
-    const listDataUserService = new ListDataUserService();
-    const listUserData = await listDataUserService.execute(request.token.id);
-
-    if (!listUserData) return response.status(401).send();
-
-    return response.json(listUserData);
   }
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
-import AuthenticateJwtMiddleware from '../middlewares/AuthenticateJwtMiddleware';
 
 const userRoutes = Router();
 
-const authenticateJwtMiddleware = new AuthenticateJwtMiddleware();
 const userController = new UserController();
 
-userRoutes.get('/', authenticateJwtMiddleware.middleware, userController.getUserData);
 userRoutes.post('/', userController.createUser);
 
 export default userRoutes;
