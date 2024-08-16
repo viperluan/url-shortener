442b573691447a79da800f52c31799a7b37de425
viperluan <viperluan@gmail.com>
2024-08-15 23:59:03 -0300

    update url routes

--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -5,13 +5,16 @@ import AuthenticateJwtMiddleware from '../middlewares/AuthenticateJwtMiddleware'
 const urlRoutes = Router();
 
 const authenticateJwtMiddleware = new AuthenticateJwtMiddleware();
+
 const urlController = new UrlController();
 
-urlRoutes.get('/:url', urlController.getShortenUrl);
+urlRoutes.head('/:shortenedUrl', authenticateJwtMiddleware.middleware, urlController.headUrl);
+urlRoutes.options('/', authenticateJwtMiddleware.middleware, urlController.optionsUrl);
 
+urlRoutes.get('/:shortenedUrl', urlController.getShortenedUrl);
 urlRoutes.get('/', authenticateJwtMiddleware.middleware, urlController.getUrls);
 urlRoutes.post('/', urlController.createUrl);
-urlRoutes.patch('/', authenticateJwtMiddleware.middleware, urlController.editUrl);
-urlRoutes.delete('/:id', authenticateJwtMiddleware.middleware, urlController.deleteUrl);
+urlRoutes.patch('/', authenticateJwtMiddleware.middleware, urlController.updateUrl);
+urlRoutes.delete('/:shortenedUrl', authenticateJwtMiddleware.middleware, urlController.deleteUrl);
 
 export default urlRoutes;
