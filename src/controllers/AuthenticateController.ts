4f81b696bad94fd3cc7862fef3de4195687af7e5
viperluan <viperluan@gmail.com>
2024-08-16 11:40:28 -0300

    added compare of hash password

--- src/controllers/AuthenticateController.ts
+++ src/controllers/AuthenticateController.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from 'express';
 import AuthenticateUserService from '../services/AuthenticateUserService';
+import GetUserWithPasswordService from '../services/GetUserWithPasswordService';
+import CompareHashedPasswordService from '../services/CompareHashedPasswordService';
 
 export default class AuthenticateController {
   async authenticate(request: Request, response: Response) {
     const { email, password } = request.body;
     if (!email || !password) return response.status(400).end();
 
+    const getUserWithPasswordService = new GetUserWithPasswordService();
+    const getUser = await getUserWithPasswordService.execute(email);
+
+    if (!getUser) return response.status(400).end();
+
+    const compareHashedPasswordService = new CompareHashedPasswordService();
+    const comparedPasswords = await compareHashedPasswordService.execute(
+      password,
+      getUser.password
+    );
+
+    if (!comparedPasswords) return response.status(400).end();
+
     const authenticateUserService = new AuthenticateUserService();
-    const authenticateToken = await authenticateUserService.execute(email, password);
+    const authenticateToken = await authenticateUserService.execute(getUser.id, getUser.email);
 
     if (!authenticateToken) return response.status(400).end();
 
