1af9076af6b513affb383d479eb3087ea09be784
viperluan <viperluan@gmail.com>
2024-08-16 00:01:52 -0300

    removed return messages

--- src/middlewares/AuthenticateJwtMiddleware.ts
+++ src/middlewares/AuthenticateJwtMiddleware.ts
@@ -17,15 +17,13 @@ export default class AuthenticateJwtMiddleware {
     const token = request.headers.authorization?.split(' ')[1];
 
     if (!token) {
-      return response
-        .status(401)
-        .json({ message: 'You must be authenticated to access this route.' });
+      return response.status(401).end();
     }
 
     const validateUserTokenService = new ValidateUserTokenService();
     const isValidToken = validateUserTokenService.execute(token);
 
-    if (!isValidToken) return response.status(401).json({ message: 'Invalid token.' });
+    if (!isValidToken) return response.status(401).end();
 
     request.token = {
       id: isValidToken.id,
