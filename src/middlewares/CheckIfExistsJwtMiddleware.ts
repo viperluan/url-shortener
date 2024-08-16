55fcd9280e2dc70719d9d264cec11e3a1fbbd83c
viperluan <viperluan@gmail.com>
2024-08-16 11:40:58 -0300

    create middleware for create url

---
+++ src/middlewares/CheckIfExistsJwtMiddleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import ValidateUserTokenService from '../services/ValidateUserTokenService';
+
+interface IAuthenticateJwtMiddlewareRequest extends Request {
+  token?: {
+    id: string;
+    email: string;
+  };
+}
+
+export default class CheckIfExistsJwtMiddleware {
+  middleware(
+    request: IAuthenticateJwtMiddlewareRequest,
+    response: Response,
+    next: NextFunction
+  ): Response | void {
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      return next();
+    }
+
+    const validateUserTokenService = new ValidateUserTokenService();
+    const isValidToken = validateUserTokenService.execute(token);
+
+    if (!isValidToken) return response.status(401).end();
+
+    request.token = {
+      id: isValidToken.id,
+      email: isValidToken.email,
+    };
+
+    return next();
+  }
+}
