ad8750f4893b4247f2718c4d8d67adf379ba12d2
viperluan <viperluan@gmail.com>
2024-08-16 11:26:30 -0300

    added hash service to user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import GetUserService from '../services/GetUserService';
+import CreateHashPasswordService from '../services/CreateHashPasswordService';
 
 export default class UserController {
   async createUser(request: Request, response: Response) {
@@ -8,13 +9,18 @@ export default class UserController {
 
     if (!email || !password) return response.status(400).end();
 
+    const createHashPasswordService = new CreateHashPasswordService();
+    const hashedPassword = await createHashPasswordService.execute(password);
+
+    if (!hashedPassword) return response.status(400).end();
+
     const getUserService = new GetUserService();
     const userExists = await getUserService.execute(email);
 
     if (userExists) return response.status(400).end();
 
     const createUserService = new CreateUserService();
-    const user = await createUserService.execute(email, password);
+    const user = await createUserService.execute(email, hashedPassword);
 
     if (!user) return response.status(400).end();
 
