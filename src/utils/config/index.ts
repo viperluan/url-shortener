ff564d1d77ca97b1cd7026a696ef54f3ae9ee6ca
viperluan <viperluan@gmail.com>
2024-08-16 00:06:10 -0300

    set default config if .env not provide

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL || '',
-  jwtSecretKey: process.env.JWT_SECRET_KEY || '',
+  databaseUrl:
+    process.env.DATABASE_URL ||
+    'postgresql://shorten_url:1234@localhost:5432/shorten_url?schema=public',
+  jwtSecretKey: process.env.JWT_SECRET_KEY || 'default_security_key',
 };
